63924dab7a730fe115a6f8d9385c73dac867c009
Xin-Ray <xxiang@mail.yu.edu>
2024-01-17 23:26:39 -0500

    add python file

--- time_flask.py
+++ time_flask.py
@@ -1,12 +1,15 @@
 from flask import Flask
-import datetime
+from datetime import datetime
+import pytz
 
 app = Flask(__name__)
 
-@app.route('/current-time', methods=['GET'])
+@app.route('/time', methods=['GET'])
 def get_current_time():
-    current_time = datetime.datetime.now().isoformat()
-    return current_time
+    # You can change the timezone as needed
+    timezone = pytz.timezone('Asia/Shanghai')
+    now = datetime.now(timezone)
+    return now.strftime('%Y-%m-%d %H:%M:%S')
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
